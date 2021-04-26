3d3316bdca11c40e831a26fb15a6352edb1355d6
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-04-26 18:42:46 +0900

    画像、メタデー、CSS #1

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,8 @@
+import "../styles/global.css";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+// _app.jsは特殊なファイルでRouteコンポーネントをラップする
+// 全ページで共通して実行させたい処理を実行する。またレイアウトも。
