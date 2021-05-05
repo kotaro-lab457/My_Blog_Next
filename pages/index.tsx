d05e4759ca07339f7350b4684b615e9303ae1b43
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-05-06 07:22:53 +0900

    header #5

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Layout, { siteTitle } from "../components/layout";
 
-import { Top } from "../components/Page/Top";
-import { Work } from "../components/Page/Work";
-import { About } from "../components/Page/About";
+import { Top } from "./Top";
 
 import { getSortedPostsData } from "../lib/posts";
 import { GetStaticProps } from "next";
@@ -36,8 +34,6 @@ export default function Home({ allPostsData }: Props) {
         <title>{siteTitle}</title>
       </Head>
       <Top />
-      <About />
-      <Work />
       <h2>Blog</h2>
       <p>ブログページの作成じゃ！</p>
       <section>
