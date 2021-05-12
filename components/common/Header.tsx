a829e9c248338c0bac4a3d87a721281729cfeb35
kotaro-lab457 <k.chanmpmp@icloud.com>
2021-05-12 23:08:23 +0900

    アイコン追加 #5

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import Link from "next/link";
 
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineCheckCircle,
+} from "react-icons/ai";
+
 const header = {
   display: "flex",
   justifyContent: "space-between",
   width: "100%",
-  padding: "0 2rem",
+  padding: "0 5rem",
 };
 
 const title = {
-  fontSize: "2rem",
+  fontSize: "3rem",
   fontFamily: "Apple Chancery",
+  margin: "0",
 };
 
 const link = {
-  margin: "20px",
+  fontSize: "1.2rem",
+  color: "#000",
+  margin: "30px 20px 20px ",
+  display: "inline-block",
+};
+
+const linkIcon = {
+  paddingRight: "5px",
+  paddingTop: "5px",
   display: "inline-block",
 };
 
@@ -25,16 +40,31 @@ export const Header: React.FC = () => {
         <h1 style={title}>Kotaro Blog</h1>
         <div>
           <Link href="/">
-            <a style={link}>Top</a>
+            <a style={link}>
+              <span style={linkIcon}>
+                <AiOutlineHome />
+              </span>
+              Top
+            </a>
           </Link>
           <Link href="/About">
-            <a style={link}>About</a>
+            <a style={link}>
+              <span style={linkIcon}>
+                <AiOutlineUser />
+              </span>
+              About
+            </a>
           </Link>
           <Link href="/Blog">
             <a style={link}>Blog</a>
           </Link>
           <Link href="/Work">
-            <a style={link}>Work</a>
+            <a style={link}>
+              <span style={linkIcon}>
+                <AiOutlineCheckCircle />
+              </span>
+              Work
+            </a>
           </Link>
         </div>
       </header>
